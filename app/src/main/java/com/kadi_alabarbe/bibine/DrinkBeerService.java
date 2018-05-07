5bd4c36515a4385746b46cb8019725661a8a9ed9
AntoninKS <antonin.kadiS@gmail.com>
2018-05-07 14:35:07 +0200

    Services

---
+++ app/src/main/java/com/kadi_alabarbe/bibine/DrinkBeerService.java
@@ -0,0 +1,80 @@
+package com.kadi_alabarbe.bibine;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DrinkBeerService extends IntentService {
+
+    private static final String ACTION_BEER = "com.kadi_alabarbe.tp1_2.action.FOO";
+    private static final String TAG = DrinkBeerService.class.getSimpleName();
+
+
+    private void copyInputStreamToFile(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DrinkBeerService() {
+        super("GetPokemonServices");
+    }
+
+    public static void startActionPoke(Context context) {
+        Intent intent = new Intent(context, DrinkBeerService.class);
+        intent.setAction(ACTION_BEER);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_BEER.equals(action)) {
+                handleActionPoke();
+            }
+        }
+    }
+
+    private void handleActionPoke() {
+        Log.d(TAG, "Thread service name:" + Thread.currentThread().getName());
+        URL url = null;
+        try {
+            url = new URL("http://binouze.fabrigi.fr/bieres.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if(HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                copyInputStreamToFile(conn.getInputStream(),
+                        new File(getCacheDir(), "bieres.json"));
+                Log.d(TAG, "Bières json downloaded !");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MainActivity.BEER_UPDATE));
+
+            }
+        }catch(MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
+++ app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
@@ -1,11 +1,26 @@
 package com.kadi_alabarbe.bibine;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BEER_UPDATE = "com.octip.cours.inf4042_11.BIERS_UPDATE";
+    private static final String TAG = DrinkBeerService.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
     public void launchActivity(View target) {
         Intent i = new Intent(this, ListActivity.class);
         startActivity(i);
+    }
+
+    public class BeerUpdate extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, getIntent().getAction());
+            getBeerFromFile();
+        }
+        // prévoir une action de notification ici
+        public JSONArray getBeerFromFile(){
+            try {
+                InputStream is = new FileInputStream(getCacheDir() + "/" + "bieres.json");
+                byte[] buffer = new byte[is.available()];
+                is.read(buffer);
+                is.close();
+                return new JSONObject(new String(buffer, "UTF-8")).getJSONArray("objects"); // construction du tableau
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new JSONArray();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return new JSONArray();
+            }
+        }
     }
 }
