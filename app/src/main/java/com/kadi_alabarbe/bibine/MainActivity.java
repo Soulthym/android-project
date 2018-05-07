e74f4934f9be2d533da8d83b712dd5473f42c14d
AntoninKS <antonin.kadiS@gmail.com>
2018-05-07 14:56:09 +0200

    Services

--- app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
+++ app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter intentFilter = new IntentFilter(BEER_UPDATE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BeerUpdate(),intentFilter);
     }
 
     public void launchActivity(View target) {
