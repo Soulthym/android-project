79381da8e426a42ad3a0ba0bd24fcb6d0d9e2fb7
AntoninKS <antonin.kadiS@gmail.com>
2018-05-24 13:36:56 +0200

    ActionBar

--- app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
+++ app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
@@ -19,6 +19,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -32,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
 //        DrinkBeerService.startActionPoke(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.credit) {
+            Toast.makeText(getApplicationContext(), "KADI Antonin_ALABARBE Thybault_2017_2018", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     public void launchListActivity(View target) {
         Intent i = new Intent(this, ListActivity.class);
         startActivity(i);
