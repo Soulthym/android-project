b028d26525ead8101f268b6a5c6d99b015380b57
AntoninKS <antonin.kadiS@gmail.com>
2018-05-25 09:38:31 +0200

    Action Bar Language

--- app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
+++ app/src/main/java/com/kadi_alabarbe/bibine/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,12 +20,12 @@ import org.json.JSONObject;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,7 +50,24 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "KADI Antonin_ALABARBE Thybault_2017_2018", Toast.LENGTH_SHORT).show();
             return true;
         }
+        if(id == R.id.francais) {
+            Locale locale = new Locale("fr-rFR");
+            Locale.setDefault(locale);
+            Configuration config= new Configuration();
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config, getBaseContext(). getResources().getDisplayMetrics());
+            Toast.makeText(getApplicationContext(), "Langue Française", Toast.LENGTH_SHORT).show();
 
+        }
+        if(id == R.id.anglais) {
+            Locale locale1 = new Locale("en-us");
+            Locale.setDefault(locale1);
+            Configuration config1= new Configuration();
+            config1.locale = locale1;
+            getBaseContext().getResources().updateConfiguration(config1, getBaseContext(). getResources().getDisplayMetrics());
+            Toast.makeText(getApplicationContext(), "English Language", Toast.LENGTH_SHORT).show();
+
+        }
         return super.onOptionsItemSelected(item);
     }
     public void launchListActivity(View target) {
